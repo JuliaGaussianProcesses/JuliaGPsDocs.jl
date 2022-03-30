150d113bad0234644dd0a1a4d28b67aa2b41fa68
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2022-03-30 13:52:22 +0200

    First writeup of the package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JuliaGPsDocs
+
+Contains all the docs mechanism used in the different JuliaGPs in particular the example generation
--- src/JuliaGPsDocs.jl
+++ src/JuliaGPsDocs.jl
@@ -1,5 +1,80 @@
 module JuliaGPsDocs
 
-# Write your package code here.
+const LITERATE = joinpath(@__DIR__, "literate.jl")
+
+function generate_examples(pkg::Module; example_dir="examples", website_root="https://juliagaussianprocesses.github.io/")
+    PKG_DIR = pkgdir(pkg)
+    EXAMPLES_DIR = joinpath(PKG_DIR, example_dir)
+    isdir(EXAMPLES_DIR) || error("example folder $EXAMPLES_DIR not found")
+
+    DOCS_DIR = joinpath(PKG_DIR, "docs")
+    EXAMPLES_OUT = joinpath(DOCS_DIR, "src", "examples")
+    ispath(EXAMPLES_OUT) && begin
+        @info "Deleting previous notebook and examples"
+        rm(EXAMPLES_OUT; recursive=true)
+    end
+    mkpath(EXAMPLES_OUT)
+
+    WEBSITE = joinpath(website_root, string(pkg) * ".jl")
+
+    examples = filter!(isdir, readdir(EXAMPLES_DIR; join=true))
+
+    precompile_packages(examples)
+
+    processes = run_examples(examples, EXAMPLES_OUT, PKG_DIR, WEBSITE)
+
+    if !isempty(processes)
+        error("no process was run, check the paths used to your examples") 
+    elseif !success(processes)
+        error("the examples $(examples[success.(processes)]) were not run successfully")
+    end
+    return examples
+end
+
+"""
+    precompile_package(examples)
+
+Go in each example and try instantiating each of the examples environments
+"""
+function precompile_package(examples::AbstractVector{<:String})
+    let script = "using Pkg; Pkg.activate(ARGS[1]); Pkg.instantiate()"
+        for example in examples
+            if !success(`$(Base.julia_cmd()) -e $script $example`)
+                error(
+                    "project environment of example ",
+                    basename(example),
+                    " could not be instantiated",
+                )
+            end
+        end
+    end
+end
+
+"""
+    run_examples(examples)
+
+Create a different process where the examples are rendered both as Markdown and Jupyter Notebooks.
+
+## Arguments
+
+- `examples` : vector of path to the examples folder
+- `EXAMPLES_OUT` : path to examples output
+- `PKG_DIR` : path to the package to be developed
+- `WEBSITE` : path to the website url
+"""
+function run_examples(examples, EXAMPLES_OUT, PKG_DIR, WEBSITE)
+    return map(examples) do example
+            return run(
+                pipeline(
+                    `$(Base.julia_cmd()) $(LITERATE) $(basename(example)) $(PKG_DIR) $(EXAMPLES_OUT) $(WEBSITE)`;
+                    stdin=devnull,
+                    stdout=devnull,
+                    stderr=stderr,
+                );
+                wait=false,
+            )::Base.Process
+    end
+end
+
 
 end
