bfa7d2dd7c1d0f5e0e53428ddf4106554736c644
ST John <st--@users.noreply.github.com>
2022-04-08 09:36:20 +0300

    clean up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,25 +2,44 @@ using JuliaGPsDocs: JuliaGPsDocs, generate_examples
 using Test
 
 @testset "JuliaGPsDocs.jl" begin
-    generate_examples(JuliaGPsDocs; examples_basedir="test/examples")
+    EXAMPLES_OUT = joinpath(pkgdir(JuliaGPsDocs), "docs", "src", "examples")
 
-    PKG_DIR = pkgdir(JuliaGPsDocs)
-    EXAMPLES_PATH = joinpath(PKG_DIR, "docs", "src", "examples")
-    # Test that the examples are generated
-    for example in ["example-a", "example-b"]
-        @test isdir(joinpath(EXAMPLES_PATH, example))
-        @test isfile(joinpath(EXAMPLES_PATH, example, "notebook.ipynb"))
-        @test isfile(joinpath(EXAMPLES_PATH, example, "index.md"))
-        @test isfile(joinpath(EXAMPLES_PATH, example, "Manifest.toml"))
+    @testset "generate examples" begin
+        generate_examples(JuliaGPsDocs; examples_basedir="test/examples")
+
+        # Test that the examples are generated
+        for example in ["example-a", "example-b"]
+            @test isdir(joinpath(EXAMPLES_OUT, example))
+            @test isfile(joinpath(EXAMPLES_OUT, example, "notebook.ipynb"))
+            @test isfile(joinpath(EXAMPLES_OUT, example, "index.md"))
+            @test isfile(joinpath(EXAMPLES_OUT, example, "Manifest.toml"))
+        end
     end
 
-    generate_examples(JuliaGPsDocs; examples_basedir="test/examples", exclusions=["example-a"])
-    @test !isdir(joinpath(EXAMPLES_PATH, "example-a"))
-    @test isdir(joinpath(EXAMPLES_PATH, "example-b"))
+    @testset "exclusions" begin
+        generate_examples(
+            JuliaGPsDocs; examples_basedir="test/examples", exclusions=["example-a"]
+        )
+        @test !isdir(joinpath(EXAMPLES_OUT, "example-a"))
+        @test isdir(joinpath(EXAMPLES_OUT, "example-b"))
+    end
 
-    generate_examples(JuliaGPsDocs; examples_basedir="test/examples", inclusions=["example-b"])
-    @test !isdir(joinpath(EXAMPLES_PATH, "example-a"))
-    @test isdir(joinpath(EXAMPLES_PATH, "example-b"))
+    @testset "inclusions" begin
+        generate_examples(
+            JuliaGPsDocs; examples_basedir="test/examples", inclusions=["example-b"]
+        )
+        @test !isdir(joinpath(EXAMPLES_OUT, "example-a"))
+        @test isdir(joinpath(EXAMPLES_OUT, "example-b"))
+    end
 
-    @test_throws ErrorException generate_examples(JuliaGPsDocs; examples_basedir="test/examples", exclusions=["example-a", "example-b"])
+    @testset "error checking" begin
+        @testset "error when no example found" begin
+            # all notebooks excluded
+            @test_throws ErrorException generate_examples(
+                JuliaGPsDocs;
+                examples_basedir="test/examples",
+                exclusions=["example-a", "example-b"],
+            )
+        end
+    end
 end
